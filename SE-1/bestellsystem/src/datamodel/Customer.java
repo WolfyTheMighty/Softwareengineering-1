fc4ba891430ee1ee0d21102d2e45eb791194e555
WolfyTheMighty <s48915@beuth-hochschule.de>
2021-10-15 12:26:18 +0200

    tests

--- SE-1/bestellsystem/src/datamodel/Customer.java
+++ SE-1/bestellsystem/src/datamodel/Customer.java
@@ -2,16 +2,17 @@ package datamodel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Class for entity type Customer. Customer is an individual (not a business) who creates and owns orders in the system.
- * 
- * @since 0.1.0
- * @version {@value package_info#Version}
+ *
  * @author Arpad Horvath
+ * @version {@value package_info#Version}
+ * @since 0.1.0
  */
 public class Customer {
-
 
 
     /**
@@ -34,132 +35,180 @@ public class Customer {
      */
     private final List<String> contacts = new ArrayList<String>();
 
+    public Customer() {
+
+    }
 
     /**
      * Public constructor with name argument.
+     *
      * @param name single-String Customer name, e.g. "Eric Meyer"
      */
-    public  Customer(String name) {
+    public Customer(String name) {
         this.id = -1;
-        splitName(name);
+        setName(name);
     }
 
     /**
      * Id getter.
+     *
      * @return customer id, may be invalid {@code < 0} if unassigned
      */
     public long getId() {
-        // TODO implement here
-        return 0;
+        return id;
     }
 
     /**
      * Id setter. Id can only be set once, id is immutable after assignment.
+     *
      * @param id assignment if id is valid {@code >= 0} and id attribute is still unassigned {@code < 0}
      * @return chainable self-reference
      */
     public Customer setId(long id) {
-        // TODO implement here
-        return null;
+        if (id > 0) {
+            this.id = id;
+        }
+        return this;
     }
 
     /**
      * Getter that returns single-String name from lastName and firstName attributes in format: {@code "lastName, firstName"} or {@code "lastName"} if {@code firstName} is empty.
+     *
      * @return single-String name
      */
     public String getName() {
-        // TODO implement here
-        return "";
+        if (firstName.isEmpty()) {
+            return lastName;
+        } else {
+            return lastName + ", " + firstName;
+        }
     }
 
     /**
      * firstName getter.
+     *
      * @return value of firstName attribute, never null, mapped to ""
      */
     public String getFirstName() {
-        // TODO implement here
-        return "";
+        if (firstName == null) return "";
+        return firstName;
     }
 
     /**
      * lastName getter.
+     *
      * @return value of lastName attribute, never null, mapped to ""
      */
     public String getLastName() {
-        // TODO implement here
-        return "";
+        if (lastName == null) return "";
+        return lastName;
     }
 
     /**
      * Setter that splits single-String name (e.g. "Eric Meyer") into first- and lastName parts and assigns parts to corresponding attributes.
+     *
      * @param name single-String name to split into first- and lastName parts
      * @return chainable self-reference
      */
     public Customer setName(String name) {
-        // TODO implement here
-        return null;
+        if (name == null) return this;
+        splitName(name);
+        return this;
     }
 
     /**
      * firstName, lastName setter. Method maintains that both attributes are never null; null-arguments are ignored and don't change attributes.
+     *
      * @param first assigned to firstName attribute
-     * @param last assigned to lastName attribute
+     * @param last  assigned to lastName attribute
      * @return chainable self-reference
      */
     public Customer setName(String first, String last) {
-        // TODO implement here
-        return null;
+        if (first == null) return this;
+        if (last == null) return this;
+        this.firstName = first;
+        this.lastName = last;
+        return this;
     }
 
     /**
      * Return number of contacts.
+     *
      * @return number of contactsContacts getter (as {@code String[]}).
      */
     public int contactsCount() {
-        // TODO implement here
-        return 0;
+
+        return contacts.size();
     }
 
     /**
      * Contacts getter (as {@code String[]}).
+     *
      * @return contacts (as {@code String[]})
      */
-    public String[ ] getContacts() {
-        // TODO implement here
-        return null;
+    public String[] getContacts() {
+        return contacts.toArray(new String[0]);
     }
 
     /**
      * Add new contact for Customer. Duplicate entries are ignored.
+     *
      * @param contact added when not already present
      * @return chainable self-reference
      */
     public Customer addContact(String contact) {
-        // TODO implement here
-        return null;
+        if (contact == null || contact.equals("")) return this;
+        if (!contacts.contains(contact)) {
+            contacts.add(contact);
+        }
+        return this;
     }
 
     /**
      * Delete i-th contact with constraint: {@code i >= 0} and {@code i < contacts.size()}, otherwise method has no effect.
+     *
      * @param i index in contacts
      */
     public void deleteContact(int i) {
-        // TODO implement here
+        if (i < contacts.size() && (i >= 0)) {
+            contacts.remove(i);
+        }
+
     }
 
     /**
      * Delete all contacts.
      */
     public void deleteAllContacts() {
-        // TODO implement here
+        contacts.clear();
     }
 
     /**
      * Split single-String name into first- and last name.
+     *
      * @param name single-String name split into first- and last name
      */
     private void splitName(String name) {
-        // TODO implement here
+        if (name == null) return;
+//        this.firstName = name.split(" |; |, ")[0];
+//        if (!(name.contains(", ") || name.contains("; "))) {
+//            name = new StringBuilder(name).reverse().toString();
+//            String temp = "";
+//            for (String s : name.split(" ")) {
+//                temp = temp + " " + new StringBuilder(s).reverse().toString();
+//            }
+//            name = temp;
+//        }
+        String split = "";
+        Pattern pattern = Pattern.compile(" |; |, ");
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.find()) {
+            split = matcher.group();
+        }
+        this.lastName = name.substring(name.lastIndexOf(split)).replace(split, "").trim();
+        name = name.replace(lastName, "");
+        this.firstName = name.trim();
+//        new StringBuilder(name).reverse().toString().split(" |; |, ")[0];
     }
 
 }
